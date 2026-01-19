35dc6dc8a15ff61c7de44069bf47fecf393f887f
Seyit Alaşhan <seyitalashan123@gmail.com>
2026-01-20 01:00:56 +0300

    .

--- Balon Patlatma Oyunu/Assets/Scripts/Score.cs
+++ Balon Patlatma Oyunu/Assets/Scripts/Score.cs
@@ -6,8 +6,17 @@ using UnityEngine.UI;
 public class Score : MonoBehaviour
 {
     public Text score_text;//En yüksek skoru yazdýrmak için gerekli deðiþken
+    public GameObject notes;
     void Start()
     {
         score_text.text = "Highest Score: " + PlayerPrefs.GetInt("HighScore").ToString();//En yüksek skoru yazdýr
+    }
+    public void Notes_Scene()
+    {
+        notes.SetActive(true);
+    }
+    public void Close_Notes()
+    {
+        notes.SetActive(false);
     }
 }
